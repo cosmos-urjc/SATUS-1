2967318200d4712f15bc9c0fa9d462022f0ae0db
PatrickeTownsend <p.townsend.2019@alumnos.urjc.es>
2025-02-26 12:23:29 +0100

    corrected issue with package bmp085

--- Air_Station/pressure.py
+++ Air_Station/pressure.py
@@ -1,13 +1,13 @@
-import adafruit_bmp085
+import Adafruit_BMP.BMP085 as BMP085
 import busio
 import time
 import board
 
 i2c1 = busio.I2C(board.SCL, board.SDA)
 
-bmp1 = adafruit_bmp085.Adafruit_BMP085_I2c(i2c1, adress = 0x76)
+bmp1 = BMP085.Adafruit_BMP085_I2c(i2c1, adress = 0x76)
 
-while True
+while True:
     temp1 = bmp1.temperature
     pressure1 = bmp1.pressure
 
