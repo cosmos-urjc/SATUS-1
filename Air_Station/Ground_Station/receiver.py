2511e965e9cd39ab9a0c31107bce93e922caff1e
Patrick Townsend Falcones <131657866+PatrickeTownsend@users.noreply.github.com>
2025-04-07 12:40:32 +0200

    LoRa receiver final python code

--- Air_Station/Ground_Station/receiver.py
+++ Air_Station/Ground_Station/receiver.py
@@ -1,33 +1,45 @@
-import time
 import board
 import busio
 import digitalio
+import cbor2
 import adafruit_rfm9x
+import time
+import serial
 
-# Define radio parameters
-RADIO_FREQ_MHZ = 868.0  # Frequency of the radio in MHz. 868.0 MHz in Europe, 915.0 MHz in North America.
-
-# Declare pins for the board
-CS = digitalio.DigitalInOut(board.CE0) 
-RESET = digitalio.DigitalInOut(board.D25)
-INT = digitalio.DigitalInOut(board.D27)
-
-
-# Initialize SPI bus
+# RFM9X LoRa radio configuration
+CS = digitalio.DigitalInOut(board.CE1)     # Chip Select (puede ser D5, D6, etc.)
+RESET = digitalio.DigitalInOut(board.D25)  # Reset del RFM95
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+RADIO_FREQ_MHZ = 868.0  # Frequency of the radio in MHz.
 
-# Initialize RFM9x radio
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, crc=False)
+# Initialize LoRa radio
+try:
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+except RuntimeError as error:
+    print('Couldnt initialize LoRa', error)
+    exit()
 
-# start receiving packages
-count=0
+print("LoRa radio initializated, waiting for data.....\n")
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)  # Ajusta la velocidad de baudios según necesites
 while True:
-        count=count+1
-        packet = rfm9x.receive() 
-        print(packet) # Wait for a packet to be received (up to 0.5 seconds)
-        if packet is not None:
-            packet_text = str(packet, 'ascii')
-            print('Received: {0}'.format(packet_text))
-        else:
-             print('Received nothing!')
-        #time.sleep(6)  # Wait for some time before sending the next command
+    packet = rfm9x.receive(timeout=5.0)  # wait up to 5 seconds for other package
+    if packet is not None:
+        try:
+            data = cbor2.loads(packet)
+            print("Data received!")
+            print(data)
+            print()
+            # Enviar datos por serial
+            try:
+                # Convertir los datos a formato string (o el formato que prefieras)
+                serial_data = packet + b'\n'  # Añade un salto de línea para separar mensajes
+                ser.write(serial_data)
+                print("Data sent via serial")
+            except Exception as serial_error:
+                print("Error sending data via serial")
+        except Exception as e:
+            print("error with CBOR protocol")
+    else:
+        print("Waiting for data.....")
+
+    time.sleep(0.03)
